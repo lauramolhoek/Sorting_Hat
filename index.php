498c4399624dfbcf679fa6c4b63623559418ef77
lauramolhoek <82821749+lauramolhoek@users.noreply.github.com>
2021-12-28 15:47:07 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="SFH/assets/css/index.css" rel="stylesheet" type="text/css">
+    <title>Document</title>
+</head>
+<body>
+    <h1> Sorting hat </h1>
+    
+    <img src="SFH/assets/image/sortinghat.jfif" alt="the sorting hat">  
+    </img> 
+
+    <br>
+    <br>
+    <br>
+    <br>
+
+    <nav>
+    
+        <div class ="add">
+            <a href="SFH/index.php">
+                Add your profile to the sorting hat.
+            </a>  <br>
+            <br>
+            <br> 
+            <a href="SFH/house.php">
+                Let the sorting hat in you take the wheel.
+            </a>  
+</div>
+  
+</nav>
+
+    
+
+
+</body>
+</html>
